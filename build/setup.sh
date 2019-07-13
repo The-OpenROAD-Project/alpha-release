276b938dc8befb717aaa63f1ccfb45b214f3a006
Tutu Ajayi <ajayi@umich.edu>
2019-07-13 14:27:31 -0400

    Adding source script and nesting installation package

---
+++ build/setup.sh
@@ -0,0 +1,33 @@
+version=@TIMESTAMP@
+modroot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/OpenROAD-${version}
+
+
+if [ ! -d "${modroot}" ]; then
+  echo "Module path does not exist: ${modroot}"
+  exit -1
+fi
+
+
+export OPENROAD=${modroot}
+export OPENROADOS=Linux-x86_64
+
+echo "OPENROAD: ${OPENROAD}"
+
+
+if [ -n "${PATH}" ]; then
+  export PATH=${modroot}/bin:${modroot}/bin/Linux-x86_64:$PATH
+else
+  export PATH=${modroot}/bin:${modroot}/bin/Linux-x86_64
+fi
+
+if [ -n "${LD_LIBRARY_PATH}" ]; then
+  export LD_LIBRARY_PATH=${modroot}/lib:${modroot}/lib/Linux-x86_64:$LD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=${modroot}/lib:${modroot}/lib/Linux-x86_64
+fi
+
+if [ -n "${MANPATH}" ]; then
+  export MANPATH=${modroot}/share/man:$MANPATH
+else
+  export MANPATH=${modroot}/share/man
+fi
