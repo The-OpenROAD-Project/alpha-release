d4c8478e7910e551a332f1b10b16327a83dee36c
Tutu Ajayi <ajayi@umich.edu>
2019-08-10 19:50:35 -0400

    Adding input/output buffering using resizer

--- flow/scripts/resize.tcl
+++ flow/scripts/resize.tcl
@@ -23,18 +23,16 @@ foreach cell $::env(DONT_USE_CELLS) {
   set dont_use_cells "$dont_use_cells [get_full_name [get_lib_cells */$cell]]"
 }
 
-# resize -buffer_cell [get_full_name [get_lib_cells */$::env(RESIZER_BUF_CELL)]] \
-#        -dont_use $dont_use_cells
-
-# DISABLED input/output buffering due to issues in CTS detail routing
-# resize -resize -repair_max_cap -repair_max_slew \
-#        -buffer_cell [get_full_name [get_lib_cells */$::env(RESIZER_BUF_CELL)]] \
-#        -dont_use $dont_use_cells
-
 # DISABLED repair max slew due to crashes running some designs
-resize -resize -repair_max_cap  \
+       # -repair_max_slew \
+
+resize -buffer_inputs \
+       -buffer_outputs \
+       -resize \
+       -repair_max_cap \
        -buffer_cell [get_full_name [get_lib_cells */$::env(RESIZER_BUF_CELL)]] \
        -dont_use $dont_use_cells
+
 
 # final report
 report_checks > $::env(REPORTS_DIR)/3_resize_checks.rpt
@@ -47,3 +45,4 @@ write_def $::env(RESULTS_DIR)/3_2_place_resized.def
 write_verilog $::env(RESULTS_DIR)/3_place.v
 
 exit
+
