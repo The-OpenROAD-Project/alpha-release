f17446cfba120fa10269dcb591fd514e4e789216
Øyvind Harboe <oyvind.harboe@zylin.com>
2019-08-28 23:31:48 +0200

    flow/harness: bump up available area for harness
    I've successfully run Yosys and RePlAce on ca. 25% of this new
    increased size area.
    
    No ill effects of having too much area for this test-harness has
    been observed.

--- flow/designs/harness.mk
+++ flow/designs/harness.mk
@@ -12,10 +12,10 @@ export GDS_FILES  = $(wildcard ./platforms/nangate45/gds/*)
 
 # These values must be multiples of placement site
 # x=0.19 y=1.4
-export DIE_AREA    = 0 0 900.22 901.6
-export CORE_AREA   = 20.14 22.4 880.46 882
-export CORE_WIDTH  = 860.32
-export CORE_HEIGHT = 859.6
+export DIE_AREA    = 0 0 9002.2 9016
+export CORE_AREA   = 20.14 22.4 8623.34 8618.4
+export CORE_WIDTH  = 8603.2
+export CORE_HEIGHT = 8596
 
 # Start with 250MHz for nangate45, relatively conservative
 export CLOCK_PERIOD = 4
